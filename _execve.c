202d7b2c8418dcc80d87cb850fc79793c22267eb
keshmuga <keshmuga@gmail.com>
2023-09-21 02:45:11 +0300

    shel

--- _execve.c
+++ _execve.c
@@ -1,93 +1,63 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * c_exit - frees user's typed command and linked list before exiting
+ * @str: user's typed command
+ * @env: input the linked list of envirnment
  */
-int _myenv(info_t *info)
+void c_exit(char **str, list_t *env)
 {
-	print_list_str(info->env);
+	free_double_ptr(str);
+	free_linked_list(env);
+	_exit(0);
+}
+
+/**
+ * _execve - execute command user typed into shell
+ * @s: command user typed
+ * @env: environmental variable
+ * @num: nth user command; to be used in error message
+ * Return: 0 on success
+ */
+int _execve(char **s, list_t *env, int num)
+{
+	char *holder;
+	int st = 0, p = 0;
+	pid_t pid;
+
+
+	if (access(s[0], F_OK) == 0)
+	{
+		holder = s[0];
+		p = 1;
+	}
+	else
+		holder = _which(s[0], env);
+	if (access(holder, X_OK) != 0)
+	{
+		not_found(s[0], num, env);
+		free_double_ptr(s);
+		return (127);
+	}
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(holder, s, NULL) == -1)
+			{
+				not_found(s[0], num, env);
+				c_exit(s, env);
+			}
+		}
+		else
+		{
+			wait(&st);
+			free_double_ptr(s);
+			if (p == 0)
+				free(holder);
+		}
+	}
 	return (0);
 }
 
-
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
-char *_getenv(info_t *info, const char *name)
-{
-	list_t *node = info->env;
-	char *p;
-
-	while (node)
-	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
-	}
-	return (NULL);
-}
-
-/**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
-int _mysetenv(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
-
-/**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
-}
